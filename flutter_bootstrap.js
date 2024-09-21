487833d81d0a92e949f9e22163bc2cc42b1f1aa9
Juan Sapién <juansapienflores@gmail.com>
2024-09-21 12:26:12 -0700

    iimage

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2287343318"
+    serviceWorkerVersion: "4164067799"
   }
 });
